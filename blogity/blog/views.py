b48d63e60a3575e47c92c4f80dff8e92189c0561
EunhaKyeong <eh0634a@gmail.com>
2020-12-02 23:55:04 +0900

    회원가입 views.py 생성

--- blogity/blog/views.py
+++ blogity/blog/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+from blog.models import Account
 
 # Create your views here.
 def index(request):
@@ -6,3 +11,25 @@ def index(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+#회원가입
+class Signup(View):
+    def post(self, request):
+        data = request.POST
+        Account.objects.create(
+            name = data['name'],
+            email = data['email'],
+            password = data['password']
+        )
+        User.objects.create_user(username=data['name'], email=data['email'], password=data['password'])
+        return redirect('/')
+
+    def get(self, request):
+        return render(request, 'signup.html')
+
+#로그인
+class Signin(View):
+    def post(self, request):
+        data = request.POST
+        print(data)
+        return redirect('/')
