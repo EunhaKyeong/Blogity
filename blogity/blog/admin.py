8da72bc9ead4cd1fba671dc573e2786dccf8dd19
EunhaKyeong <eh0634a@gmail.com>
2020-12-02 23:53:37 +0900

    Account model 생성

--- blogity/blog/admin.py
+++ blogity/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Account
 
-# Register your models here.
+#Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'password']
+
+admin.site.register(Account, AccountAdmin)
--- blogity/blog/models.py
+++ blogity/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=30, unique=True)
+    password = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'users'
