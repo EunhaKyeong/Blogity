97409c2d57903a45c58a6ec6454081fa80d7e38e
EunhaKyeong <eh0634a@gmail.com>
2020-12-02 23:54:33 +0900

    로그인/로그아웃/회원가입 url 경로 추가

--- blogity/blog/urls.py
+++ blogity/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='../templates/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.Signup.as_view(), name='signup'),
 ]
